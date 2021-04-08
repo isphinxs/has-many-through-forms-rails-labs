533194e90218755ae5f74fe7eb79d5f0d4326ec8
Samantha Ostrowski <isphinxs@hotmail.com>
2021-04-08 19:44:55 -0400

    commit tree 47ce256002b231543750f84fb491a398a44187d6

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,10 +6,12 @@ class Post < ActiveRecord::Base
   accepts_nested_attributes_for :categories
 
   def categories_attributes=(category_attributes)
-    category_attributes.values.each do |category_attribute|
+    category_attributes.values.each do |category_attribute| 
       # byebug
-      category = Category.find_or_create_by(category_attribute)
-      self.categories << category
+      if !category_attribute[:name].blank?  
+        category = Category.find_or_create_by(category_attribute)
+        self.categories << category
+      end
     end
   end
 end
